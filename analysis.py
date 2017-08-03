d98bf0bca79db7ece2e48cec71a301fbbae65029
Minh Nguyen <minh.nguyen98@gmail.com>
2017-08-03 14:42:56 +0700

    Annotated code

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+"""Review analysis"""
 import sys
 import json
 import csv
@@ -10,87 +11,124 @@ from google.cloud.proto.language.v1beta2 import language_service_pb2
 from google.cloud import translate
 
 class EntitySentimentAnalysis():
+    """Class for analyzing reviews"""
     def __init__(self):
+        # Client to Natural Language API (v1beta2)
         self.language_client = language_service_client.LanguageServiceClient()
+        # Client to Translate API
         self.translate_client = translate.Client()
-        self.encoding = enums.EncodingType.UTF16 if sys.maxunicode == 65535 else enums.EncodingType.UTF32
+        # Encoding to pass to API
+        self.encoding = enums.EncodingType.UTF16 if sys.maxunicode == 65535 \
+                                                 else enums.EncodingType.UTF32
+        # Open config file
         with open(sys.argv[1]) as config:
             config_json = json.load(config)
+            # Data, topic, and classification information
             self.data = config_json['data']
             self.topics = config_json['topics']
             self.sentiment_classes = config_json['classes']
 
     def analyze_reviews(self):
+        """Analyzes reviews using NL API and Translate API"""
         with open(self.data, 'r') as csvfile:
+            # CSV reader
             review_reader = csv.DictReader(csvfile)
             with open('sentiment.csv', 'w') as outfile:
-                writer_fieldnames = review_reader.fieldnames + ['translated_text', 'parent_topic', 'topic', 'sentiment']
+                # Headers for output CSV file
+                writer_fieldnames = review_reader.fieldnames + \
+                                    ['translated_text', 'parent_topic', 'topic', 'sentiment']
+                # CSV writer to output
                 review_writer = csv.DictWriter(outfile, fieldnames=writer_fieldnames)
+                # Write header
                 review_writer.writeheader()
+                # Go through each review
                 for i, row in enumerate(review_reader):
+                    # Go through all reviews
                     if int(sys.argv[2]) == 0:
                         pass
-                    elif i > int(sys.argv[2]):
+                    # Go up to specified review
+                    elif i >= int(sys.argv[2]):
                         break
-
+                    # Print review number and part of the review text
                     print(i, row['text'][:30]+'...')
 
+                    # Translate non-english reviews to english
+                    # and add to output row
                     translated_text = row['text']
                     if row['detected_lang'] != 'en':
-                        translated_text = self.translate_client.translate(row['text'])['translatedText']
+                        translation = self.translate_client.translate(row['text'])
+                        translated_text = translation['translatedText']
                     row['translated_text'] = translated_text
 
+                    # Entity sentiment analysis
                     sentiments = self.entity_sentiment(translated_text)
+                    # If any relevant entities identified
                     if sentiments:
                         for parent_topic in sentiments:
                             for topic in sentiments[parent_topic]:
+                                # Write row with topic and sentiment information
                                 row['parent_topic'] = parent_topic
                                 row['topic'] = topic
                                 row['sentiment'] = sentiments[parent_topic][topic]
                                 review_writer.writerow(row)
+                    # Default information to 'unknown'
                     else:
                         row['parent_topic'] = 'unknown'
                         row['topic'] = 'unknown'
                         row['sentiment'] = 'unknown'
                         review_writer.writerow(row)
 
-    def entity_sentiment(self, text):
+    def entity_sentiment(self, text: str):
+        """Identifies relevant entities and records sentiment"""
         if isinstance(text, six.binary_type):
             text = text.decode('utf-8')
 
+        # Create document, set content, and type
         document = language_service_pb2.Document()
         document.content = text.encode('utf-8')
         document.type = enums.Document.Type.PLAIN_TEXT
 
+        # Result from API call
         result = self.language_client.analyze_entity_sentiment(
             document, self.encoding)
-
+        # Nested dictionary to hold parent topic, topic, and sentiment
         sentiments = defaultdict(partial(defaultdict, float))
+        # Counter for appearances of each topic for normalization
         topic_counter = Counter()
+        # Go through each entity
         for entity in result.entities:
+            # If sentiment is present
             if entity.sentiment.score != 0 and entity.sentiment.magnitude != 0:
+                # Go through each parent topic
                 for parent_topic in self.topics:
+                    # Go through each subtopic
                     for topic in self.topics[parent_topic]:
+                        # If topic present in entity
                         if topic in entity.name.lower():
+                            # Add to dictionary
                             sentiments[parent_topic][topic] += entity.sentiment.score
+                            # Add to counter
                             topic_counter[topic] += 1
 
         def classify(self, val):
+            """Classifies entity sentiment by score"""
             for sent_class in self.sentiment_classes:
                 class_range = self.sentiment_classes[sent_class]
                 if class_range['min'] <= val and val < class_range['max']:
                     return sent_class
             return None
 
-        sentiments = {pt:
-                        {
-                            t:classify(self, sentiments[pt][t] / topic_counter[t])
-                            for t in sentiments[pt]
-                        } for pt in sentiments}
+        # Normalize sentiment scores and classify
+        sentiments = {pt:{t:classify(self, sentiments[pt][t] / topic_counter[t])
+                          for t in sentiments[pt]} for pt in sentiments}
         return sentiments
 
-if __name__ == '__main__':
+def main():
+    """Review analysis logic"""
     assert len(sys.argv) == 3, "Usage: python newanalysis.py <configname.json> <# of reviews>"
     assert sys.argv[2].isdigit(), "<# of reviews> has to be an integer"
     analyzer = EntitySentimentAnalysis()
     analyzer.analyze_reviews()
+
+if __name__ == '__main__':
+    main()
